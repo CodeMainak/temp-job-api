f6b198d5edce30a3f110d4b3e87ca1709b9970dc
Mainak Mondal <mainak.mondal33@gmail.com>
2024-06-04 00:10:33 +0530

    fixed register issue

--- models/User.js
+++ models/User.js
@@ -33,9 +33,9 @@ UserSchema.pre('save', async function () {
 UserSchema.methods.createJWT = function () {
   return jwt.sign(
     { userId: this._id, name: this.name },
-    `${process.env.JWT_SECRET}`,
+    process.env.JWT_SECRET,
     {
-      expiresIn:`${process.env.JWT_LIFETIME}`,
+      expiresIn: process.env.JWT_LIFETIME,
     }
   )
 }
